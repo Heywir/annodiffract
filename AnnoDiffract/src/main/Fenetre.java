606ede0beb33e5e5b32cd2b04c164a7d464992af
Heywir <heywir@hotmail.fr>
2017-05-25 13:36:05 +1100

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of https://github.com/Heywir/annodiffract.git
    
    Conflicts:
    	AnnoDiffract/src/main/Fenetre.java

--- AnnoDiffract/src/main/Fenetre.java
+++ AnnoDiffract/src/main/Fenetre.java
@@ -1,15 +1,48 @@
 package main;
 
-public class Fenetre {
+import java.awt.BorderLayout;
 
-	int i = 0;
-	//
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+
+public class Fenetre extends JFrame{
+
+	JFrame mainWindow = null;
+	JPanel mainPanel = null;
+	JMenuBar mainMenuBar = null;
+	JMenu menuFile = null;
+	
 	public Fenetre() {
 	
-		//
+		mainWindow = new JFrame("AnnoDiffract");
+		mainPanel = new JPanel();
+		mainMenuBar = new JMenuBar();
+		menuFile = new JMenu();
+		
+		// Layout
+		
+		BorderLayout layout = new BorderLayout();
+		this.setLayout(layout);
+		
+		// Window Settings
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		
+		// Ajouts
+		
+		this.add(mainMenuBar, BorderLayout.NORTH);
+		this.add(mainPanel, BorderLayout.CENTER);
+		
 	}
 	
 	public static void main(String[] args) {
-		//
+		
+		Fenetre window = new Fenetre();
+		window.setVisible(true);
+		
 	}
 }
