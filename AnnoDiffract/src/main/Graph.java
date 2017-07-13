5680ba60dfdb1f1a0899edcbef370cfe478ea00b
Morteum <dawnitosky@gmail.com>
2017-07-13 12:09:03 +1100

    Graph gets better

--- AnnoDiffract/src/main/Graph.java
+++ AnnoDiffract/src/main/Graph.java
@@ -47,7 +47,6 @@ public class Graph extends JFrame implements ChartMouseListener{
         plot.setRangeCrosshairVisible(true);        
         
         XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer( );
-        renderer.setser
         renderer.setSeriesPaint( 0 , Color.BLACK );
         renderer.setSeriesOutlineStroke(0, new BasicStroke(0.1f));
         renderer.setSeriesStroke( 0 , new BasicStroke( 1.0f ) );
